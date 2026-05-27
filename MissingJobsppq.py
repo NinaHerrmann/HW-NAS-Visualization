12a9b71890f377170441f43f1e5b2b633968b809
Nina Herrmann <nina.herrmann@uni-muenster.de>
2026-05-27 15:34:01 +0200

    try to differentiate causes

--- MissingJobsppq.py
+++ MissingJobsppq.py
@@ -24,15 +24,6 @@ def build_parser():
     p.add_argument('-file',
                    type=str,
                    help='file that has combination as lines')
-    p.add_argument('--modelpath',
-                   type=str,
-                   help='file to store models')
-    p.add_argument('--weightpath',
-                   type=str,
-                   help='folder where weights are stored')
-    p.add_argument('--resultpath',
-                   type=str,
-                   help='resultfile for accuracy')
     return p
 
 def parse_args():
@@ -67,9 +58,9 @@ hw_api = HWAPI("HW-NAS-Bench-v1_0.pickle", search_space="nasbench201")
 all_data = []
 args = parse_args()
 file = args.file
-model_path = args.modelpath
-resultpath = args.resultpath
-weightpath = args.weightpath
+model_path = "/scratch/tmp/n_herr03/NATS_Benchmark/models/espdl"
+resultpath = "/scratch/tmp/n_herr03/hwnas/result"
+weightpath = "/scratch/tmp/n_herr03/NATS_Benchmark/NATS-tss-v1_0-3ffb9-full"
 
 mean = (0.4914, 0.4822, 0.4465)
 std  = (0.2470, 0.2435, 0.2616)
@@ -77,7 +68,6 @@ std  = (0.2470, 0.2435, 0.2616)
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize(mean, std)])
-#print(f"read data {resultpath}/data")
 testset = torchvision.datasets.CIFAR10(root=f'{resultpath}/data', train=False, download=True, transform=transform)
 
 batchsize = 1
@@ -110,7 +100,7 @@ with open(file, "r", encoding="utf-8") as f:
                 with bz2.BZ2File(weights_path, "rb") as f:
                     data = pickle.load(f)
             except FileNotFoundError:
-                with open("/home/n/n_herr03/HW-NAS-Visualization/doesnotwork.txt", "a") as f:
+                with open("/home/n/n_herr03/HW-NAS-Visualization/noweight.txt", "a") as f:
                     f.write(f"{idx}\n")
                 continue
             validkey = []
@@ -118,8 +108,9 @@ with open(file, "r", encoding="utf-8") as f:
                 if key in data: validkey.append(key)
 
             if not validkey:
-                print("No valid data found")
-                exit()
+                with open("/home/n/n_herr03/HW-NAS-Visualization/novalidkey.txt", "a") as f:
+                    f.write(f"{idx}\n")
+                continue
 
             key = max(validkey)
             for innerkey in data[key]["all_results"]:
@@ -128,29 +119,6 @@ with open(file, "r", encoding="utf-8") as f:
                 else:
                     with open("/home/n/n_herr03/HW-NAS-Visualization/noseed.txt", "a") as f:
                         f.write(f"{idx}\n")
-
-
-                ourdict = data[key]["all_results"][('cifar10', checkseed)]["net_state_dict"]
-                print(ourdict)
-                network = get_cell_based_tiny_net(netconfig)
-                network.load_state_dict(ourdict)
-                x = torch.rand([1, 3, 32, 32], dtype=torch.float32)
-                network.eval()
-                init_end = time.process_time()
-                acc = evaluate_top1(network, calib_loader)
-                transform_start = time.process_time()
-                quant_ppq_graph = espdl_quantize_torch(network, f"{model_path}/espdl/model{idx}_{seed}.espdl",
-                                                    collate_fn=collate_x_only, calib_dataloader=calib_loader, calib_steps=32,
-                                                    error_report=False, verbose=0,
-                                                    input_shape=[batchsize, 3, 32, 32])  # setting=quant_setting)
-                executor = TorchExecutor(quant_ppq_graph, device='cpu')
-                dataset = calib_loader.dataset
-                transform_end = time.process_time()
-                accqu = evaluate_top1(executor, calib_loader)
-                if not os.path.exists(f'{resultpath}/result.csv'):
-                    with open('result.csv', 'a', encoding='utf-8') as f:
-                        f.write("idx,seed,dataset,recorded_test_acc,quant_test_acc\n")
-                line = f"{idx},{checkseed },cifar10,{acc * 100:.2f},{accqu * 100:.2f}\n"
-                print(line)
-                with open(f'{resultpath}/result.csv', 'a', encoding='utf-8') as f:
-                    f.write(line)
+                    continue
+                with open(f'{resultpath}/shouldwork.txt', 'a', encoding='utf-8') as f:
+                    f.write(f"{idx},{innerkey}\n")
